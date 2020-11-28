53d139076c196f60f5820c003544bc1167231460
Vilius Šapauskas <v.sapauskas.1997@gmail.com>
2020-11-28 18:39:39 +0200

    Create DiscountCoupon.cs

---
+++ src/FCT.Persistence/Entities/DiscountCoupon.cs
@@ -0,0 +1,9 @@
+﻿namespace FCT.Persistence.Entities
+{
+    public class DiscountCoupon
+    {
+        public int Id { get; set; }
+        public string CouponCode { get; set; }
+        public int DiscountPercentage { get; set; }
+    }
+}
