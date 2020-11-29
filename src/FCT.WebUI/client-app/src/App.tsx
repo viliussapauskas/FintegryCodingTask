aee229f012be7b1548725d25d083f5f5ef0043b2
Vilius Šapauskas <v.sapauskas.1997@gmail.com>
2020-11-29 11:54:43 +0200

    Update App.tsx

--- src/FCT.WebUI/client-app/src/App.tsx
+++ src/FCT.WebUI/client-app/src/App.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import RegisterPage from "./pages/RegisterPage";
 
 const App = () => {
+  useEffect(() => {
+    var users = localStorage.getItem("users");
+
+    if (!users) {
+      localStorage.setItem("users", JSON.stringify([]));
+    }
+  }, []);
   return (
-    <div className="App"></div>
+    <div className="App">
+      <main>
+        <Router>
+          <Switch>
+            <Route exact path="/register" component={RegisterPage} />
+            <Route path="/" component={HomePage} />
+          </Switch>
+        </Router>
+      </main>
+    </div>
   );
-}
+};
 
 export default App;
